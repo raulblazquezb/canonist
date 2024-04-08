97ee3748f71476022d16d02d9843b5cf07bd29c8
jorge <202315347@alu.comillas.edu>
2024-04-08 17:18:18 +0200

    prueba

--- streamlit/intro_app/cnn.py
+++ streamlit/intro_app/cnn.py
@@ -7,6 +7,7 @@ import torchvision
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from results import Results
 from tempfile import TemporaryDirectory
 import json
 
